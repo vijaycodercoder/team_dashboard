8af6e71b9248fdeb388669957497914d2414b5e9
vijay vijay <vijaydrive14@gmai.com>
2025-09-20 14:28:39 +0530

    done

--- src/components/MemberModal.tsx
+++ src/components/MemberModal.tsx
@@ -96,15 +96,15 @@ const avatarFallback = useMemo(() =>
           <Card >
             <CardHeader>
               <CardDescription>
-           <div className="space-y-2 text-le">
-                <label className="text-sm font-semibold text-gray-700">Bio</label>
-                <p className="text-gray-600 leading-relaxed bg-gray-50 p-4 rounded-lg border">
+           <div className="space-y-2 text-le ">
+                <label className="text-sm font-semibold dark:text-white  text-gray-700">Bio</label>
+                <p className="text-gray-600 leading-relaxed bg-gray-50 p-4 rounded-lg my-2 border">
                {modelData.bio}
                 </p>
               </div>
               <div className="flex flex-col gap-4">
                 <div className="space-y-2">
-                  <label className="text-sm font-semibold text-gray-700 flex items-center gap-2">
+                  <label className="text-sm font-semibold my-2 dark:text-white text-gray-700 flex items-center gap-2">
                     <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                     </svg>
@@ -116,7 +116,7 @@ const avatarFallback = useMemo(() =>
                 </div>
 
                 <div className="space-y-2">
-                  <label className="text-sm font-semibold text-gray-700 flex items-center gap-2">
+                  <label className="text-sm font-semibold dark:text-white text-gray-700 flex items-center gap-2">
                     <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                     </svg>
@@ -142,8 +142,8 @@ const avatarFallback = useMemo(() =>
           <Card>
             <CardHeader>
               <CardDescription>
-                <label className="text-sm font-semibold text-gray-700">Projects</label>
-              <ul className="list-disc pl-5 space-y-1 text-gray-600 leading-relaxed bg-gray-50 p-4 rounded-lg border ">
+                <label className="text-sm font-semibold  dark:text-white  text-gray-700 ">Projects</label>
+              <ul className="list-disc pl-5 space-y-1 text-gray-600 leading-relaxed my-2 bg-gray-50 p-4 rounded-lg border ">
   {modelData.projects.map((project, i) => (
     <li className="mx-3" key={i}>{project}</li>
   ))}
